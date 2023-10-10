ff4aebfefe0b265bf2d64011d31478a1c76cae3e
daoatzy <dafasetiadi65@gmail.com>
2023-10-10 20:21:34 +0700

    modify jatah cuti & type t

---
+++ src/DoWhileCuti23.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class DoWhileCuti23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int  jatahCuti, jumlahHari ;
+        String konfirmasi;
+
+        System.out.println("Jatah cuti : ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.println("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+            
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.println("Jumlah hari : ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                }
+                else{
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    continue;
+                }
+            }
+            else if(konfirmasi.equalsIgnoreCase("t")){
+                System.out.println("Program berhenti");
+                break;
+            }
+        }
+         while (jatahCuti > 0);
+         sc.close();
+    }
+}
